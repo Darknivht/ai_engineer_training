5ba8da9ca5766269c2b2649e74e5a742d3c5974b
unknown <mohammedauwal2007@gmail.com>
2026-04-14 16:59:00 +0100

    project 4 segment2 run1

---
+++ CS50_Python/CS50_lecture0_functions_and_variables/run_2/segment2/receipt.py
@@ -0,0 +1,36 @@
+"""Task: Write receipt.py that:
+
+Asks the user for a store name, an item name, and its price (as a string for now — no math required yet)
+Prints a formatted receipt using what you know about print():
+================================
+        SHOP RITE
+================================
+Item:       Laptop
+Price:      $1200.00
+--------------------------------
+Thank you for shopping with us!
+================================
+Requirements:
+
+Use print("=" * 32) for separator lines
+Use end="" at least once to control line endings
+Use sep at least once
+Use an f-string for at least one line
+The store name must be cleaned (stripped, title-cased) from user input
+Use escape characters if needed (e.g., if the store name contains a quote)"""
+
+# Ask user for store name, item name and item price
+store_name = input("Enter Store Name: ").strip().upper()
+item_name = input("Enter Item Name: ").strip().title()
+item_price = input("Enter Item Price: ").strip()
+
+WIDTH = 32
+
+# Print receipt
+print("=" * WIDTH)
+print(f"{store_name.center(WIDTH)}")
+print("=" * WIDTH)
+print(f"Item:       {item_name}")
+print(f"Price:      ${item_price}")
+print("-" * WIDTH)
+print("Thank you for shopping with us!", end="\n" + "=" * WIDTH)
